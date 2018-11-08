9082bcb92a9b0066b23b55e04518e4cea19d8e38
Roland Parnaso <Roland Parnaso>
2018-11-08 13:42:39 -0800

    hotfix cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,7 +6,7 @@
 # Read more: https://github.com/cyu/rack-cors
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allowed_origins = (ENV['RAILS_ENV'] == 'production') ? 'https://nodebuckshq.com' : '*'
+  allowed_origins = (ENV['RAILS_ENV'] == 'production') ? ['https://nodebucks.com', 'https://nodebuckshq.com'] : '*'
 
   allow do
     origins allowed_origins
