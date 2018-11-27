1d77745f1f13d87d9d0666c98123289deaf385c9
Kolosek <office@kolosek.com>
2018-11-27 11:14:58 +0100

    Added more coverage for users controller actions

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -109,4 +109,168 @@ RSpec.describe UsersController, type: :controller do
       expect(User.find(user.slug).address).to eq 'Address2'
     end
   end
+
+  describe "PATCH 'profile'" do
+    let!(:user) { FactoryBot.create(:user, password: '123', password_confirmation: '123', address: 'Address1') }
+
+    before(:each) do
+      patch :profile, params: { user: { address: 'Address2'}, user_slug: user.slug }, format: :json
+    end
+
+    it "returns a successful 200 response" do
+      expect(response).to be_successful
+    end
+
+    it "updates user profile" do
+      expect(User.find(user.slug).address).to eq 'Address2'
+    end
+  end
+
+  describe "PATCH 'reset'" do
+    let!(:user) { FactoryBot.create(:user, email: 'user1@domain.com') }
+
+    before(:each) do
+      patch :reset, params: { email: 'user1@domain.com', user_slug: user.slug }, format: :json
+    end
+
+    it "returns a successful 200 response" do
+      expect(response).to be_successful
+    end
+
+    it "creates reset token" do
+      expect(User.find(user.slug).reset_token).not_to be_empty
+    end
+  end
+
+  describe "PATCH 'reset_password'" do
+    let!(:user) { FactoryBot.create(:user, email: 'user1@domain.com') }
+
+    before(:each) do
+      patch :reset, params: { email: 'user1@domain.com', user_slug: user.slug }, format: :json
+      patch :reset_password, params: { user_slug: user.reload.reset_token, user: { password: '123', password_confirmation: '123'} }, format: :json
+    end
+
+    it "returns a successful 200 response" do
+      expect(response).to be_successful
+    end
+
+    it "sets new user password" do
+      parsed_response = JSON.parse(response.body)
+      expect(parsed_response['message']).to eq 'Password has been updated.'
+    end
+  end
+
+  describe "POST 'admin_login'" do
+    let!(:user) { FactoryBot.create(:user, email: 'admin@domain.com', password: '123', password_confirmation: '123', admin: true) }
+
+    before(:each) do
+      post :admin_login, params: { email: 'admin@domain.com', password: '123'} , format: :json
+    end
+
+    it "returns a successful 200 response" do
+      expect(response).to be_successful
+    end
+  end
+
+  describe "GET 'balance'" do
+    let!(:user) { FactoryBot.create(:user, email: 'admin@domain.com', password: '123', password_confirmation: '123', admin: true) }
+
+    before(:each) do
+      controller.class.skip_before_action :authenticate_request, raise: false
+      allow(controller).to receive(:current_user).and_return(FactoryBot.create(:user))
+      get :balance, format: :json
+    end
+
+    it "returns a successful 200 response" do
+      expect(response).to be_successful
+    end
+
+    it "returns user data" do
+      parsed_response = JSON.parse(response.body)
+      expect(parsed_response).not_to be_empty
+    end
+  end
+
+  describe "GET 'referrer'" do
+    let!(:user) { FactoryBot.create(:user, email: 'admin@domain.com', password: '123', password_confirmation: '123', admin: true) }
+
+    before(:each) do
+      allow(controller).to receive(:current_user).and_return(FactoryBot.create(:user))
+      get :referrer, format: :json
+    end
+
+    it "returns referrer data" do
+      parsed_response = JSON.parse(response.body)
+      expect(parsed_response).not_to be_empty
+    end
+  end
+
+  describe "GET 'confirm'" do
+    let!(:user) { FactoryBot.create(:user, confirmed_at: nil ) }
+
+    before(:each) do
+      get :confirm, params: { user_slug: user.slug }, format: :json
+    end
+
+    it "updates confirmed_at field" do
+      expect(User.find(user.slug).confirmed_at).not_to be nil
+    end
+  end
+
+  # TODO: Possibly broken action
+  # describe "GET 'verify'" do
+  #   let!(:user) { FactoryBot.create(:user, email: 'user1@domain.com' ) }
+
+  #   before(:each) do
+  #     get :verify, params: { user_slug: user.slug, user: { new_email: 'user2@domain.com'} }, format: :json
+  #   end
+
+  #   it "sets new email" do
+  #     expect(User.find(user.slug).email).to eq 'user2@domain.com'
+  #   end
+  # end
+
+  describe "DELETE 'destroy'" do
+    let!(:user) { FactoryBot.create(:user) }
+
+    before(:each) do
+      controller.class.skip_before_action :authenticate_request, raise: false
+      allow(controller).to receive(:current_user).and_return(user)
+      delete :destroy, params: { slug: user.slug }, format: :json
+    end
+
+    it "returns a successful 200 response" do
+      expect(response).to be_successful
+    end
+
+    it "removes current user" do
+      parsed_response = JSON.parse(response.body)
+      expect(parsed_response['message']).to eq 'User account has been removed.'
+    end
+  end
+
+  describe "GET 'show'" do
+    let!(:user) { FactoryBot.create(:user) }
+
+    before(:each) do
+      get :show, params: { slug: user.slug }, format: :json
+    end
+
+    it "returns user data" do
+      parsed_response = JSON.parse(response.body)
+      expect(parsed_response).not_to be_empty
+    end
+  end
+
+  describe "POST 'password_confirmation'" do
+    let!(:user) { FactoryBot.create(:user, password: '123', password_confirmation: '123') }
+
+    before(:each) do
+      post :password_confirmation, params: { user_slug: user.slug, user: { password: '123' } }, format: :json
+    end
+
+    it "returns a successful 200 response" do
+      expect(response).to be_successful
+    end
+  end
 end
