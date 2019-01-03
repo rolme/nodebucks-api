01f6d5167013d3950fadfe5249f0ccd4f938bf11
Roland Parnaso <Roland Parnaso>
2019-01-03 09:41:34 -0800

    Make sure to get all rewards

--- app/controllers/rewards_controller.rb
+++ app/controllers/rewards_controller.rb
@@ -2,6 +2,6 @@ class RewardsController < ApplicationController
   before_action :authenticate_request, only: [:index]
 
   def index
-    @rewards ||= Reward.where(id: Node.where(user_id: current_user.id, deleted_at: nil, status: ['offline', 'online']).select(:id)).order(timestamp: :desc)
+    @rewards ||= Reward.where(node_id: Node.where(user_id: current_user.id, deleted_at: nil, status: ['offline', 'online']).select(:id).map(&:id)).order(timestamp: :desc)
   end
 end
